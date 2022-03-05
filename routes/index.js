37cfd31fec4c3d1eaed328d8c981d391f00f0f73
zhangyang <zhangyang01@tongtech.com>
2022-03-05 19:33:04 +0800

    123

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,15 @@
 var express = require('express');
 var router = express.Router();
-
+let app=express();
 /* GET home page. */
+
+
+
+
 router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
 router.post('/getHomeCarousel', function (req, res, next) {
   // 可以通过req.body来获取post传递的键值对	
   let data = {
@@ -16,22 +21,20 @@ router.post('/getHomeCarousel', function (req, res, next) {
     msg: 'ok',
     data: data
   });
-
 });
 
-router.post('/postImg', function (req, res, next) {
-  console.log(req.fields)
+const multer = require('multer');
+const upload=multer({dest:'upload/'})
+
+
+router.post( "/postImg", upload.single('aa'), ( req, res ) => {
+  console.log(req.body)
   console.log(req.files)
-  console.log(res);
-  let data = {
-    name: 'xhm',
-    age: 12
-  }
-  res.json({
-    code: 0,
-    msg: 'ok',
-    data:data
-  });
-});
+  res.json(
+    {
+      msg:'ok'
+    }
+  );
+} );
 
 module.exports = router;
