ef41de7dee59ea589c0d1a7b029ff4cc63651562
zhangyang110 <798194200@qq.com>
2022-03-06 11:08:11 +0800

    123

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
+
 /* GET home page. */
 router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
@@ -19,10 +20,43 @@ router.post('/getHomeCarousel', function (req, res, next) {
 
 });
 
-router.post('/postImg', function (req, res, next) {
-  console.log(req.fields)
-  console.log(req.files)
-  console.log(res);
+
+
+
+
+
+var multer  = require('multer')
+// var upload = multer({ dest: 'uploads/' })
+var fs = require('fs')
+
+let upload = multer({
+  storage: multer.diskStorage({
+      destination: function (req, file, cb) {
+          cb(null, './uploads/');
+      },
+      filename: function (req, file, cb) {
+          var changedName = (new Date().getTime())+'-'+file.originalname;
+          cb(null, changedName);
+      }
+  })
+});
+
+router.post('/postImg', upload.array('file',12),(req, res, next)=> {  
+  try{
+    // console.dir(JSON.parse(req.body.file))
+    let file = req.file;
+    console.log(file)
+    //存储上传对象信息
+    let fileInfo = {};
+    //修改名字，第一个参数为旧路径，第二个参数为新路径（注意：旧路径要和上面的dest保持一致）
+    // fs.renameSync('./public/upload_tmp/' + file.filename, './public/images/' + file.originalname);
+    // 获取文件信息
+    // fileInfo.mimetype = file.mimetype;
+    // fileInfo.originalname = file.originalname;
+    // fileInfo.size = file.size;
+    // fileInfo.path = file.path;
+  }catch(err){
+  }
   let data = {
     name: 'xhm',
     age: 12
